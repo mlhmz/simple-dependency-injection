0ba55d28973895a52e05261552a946e433e1a2c2
Malek Hamza <malek.hamza@gmx.de>
2023-12-19 21:39:34 +0100

    chore: Improve Injectable Documentation

--- src/main/java/xyz/mlhmz/Injectable.java
+++ src/main/java/xyz/mlhmz/Injectable.java
@@ -4,14 +4,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * The injectable annotation is responsive for marking classes to be put into the IoC Container
+ * The injectable annotation is responsive for marking classes to be put into the {@link InjectableContainer}
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Injectable {
     /**
      * Defines if the Interface of the Injectable should be resolved.
      * <br/>
-     * If enabled, the class can be fetched out of the IoC Container by the interface
+     * If enabled, the class can be fetched out of the {@link InjectableContainer} by the interface
      */
     boolean resolveIfc() default true;
 }
