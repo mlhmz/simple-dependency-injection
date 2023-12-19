3662a98681028bda074a74c83df4b901a751ea73
Malek Hamza <malek.hamza@gmx.de>
2023-12-19 21:34:24 +0100

    feat: Add injectable annotation for marking interfaces

---
+++ src/main/java/xyz/mlhmz/Injectable.java
@@ -0,0 +1,17 @@
+package xyz.mlhmz;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * The injectable annotation is responsive for marking classes to be put into the IoC Container
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Injectable {
+    /**
+     * Defines if the Interface of the Injectable should be resolved.
+     * <br/>
+     * If enabled, the class can be fetched out of the IoC Container by the interface
+     */
+    boolean resolveIfc() default true;
+}
